d4968eafc93987b06fd108f7fea3dc6de622878a
Артём Васильевичь <megobyt2000@mail.ru>
2023-10-04 16:54:58 +0300

    TestTask

---
+++ TestTask/Services/EF/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.EF
+{
+    public class OrderService : IOrderService
+    {
+        public readonly ApplicationDbContext dbContext;
+        public OrderService(ApplicationDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+        public async Task<Order> GetOrder()
+        {
+            var order = dbContext.Orders.OrderByDescending(o => o.Price).FirstOrDefault();
+            return order;
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            var orders = dbContext.Orders.Where(o => o.Quantity > 10);
+            return orders.ToList();
+        }
+    }
+}
---
+++ TestTask/Services/EF/UserService.cs
@@ -0,0 +1,27 @@
+﻿using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.EF
+{
+    public class UserService : IUserService
+    {
+        public readonly ApplicationDbContext dbContext;
+        public UserService(ApplicationDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+        public async Task<User> GetUser()
+        {
+            var user = dbContext.Users.OrderByDescending(x => x.Orders.Count).FirstOrDefault();
+
+            return user;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var users = dbContext.Users.Where(x => x.Status == Enums.UserStatus.Inactive);
+            return users.ToList();
+        }
+    }
+}
